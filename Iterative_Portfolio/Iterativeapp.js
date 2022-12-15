8ca309dcebdacd54e51a0ce77d455da8fc38f49a
@f4sxkdn1kp <90721727+f4sxkdn1kp@users.noreply.github.com>
2022-12-15 13:47:39 -0500

    RR final commit

--- Iterative_Portfolio/Iterativeapp.js
+++ Iterative_Portfolio/Iterativeapp.js
@@ -33,5 +33,4 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+
