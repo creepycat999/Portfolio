86642cfe367eb54a4093959758da3e1b62cb9f2d
@f4sxkdn1kp <90721727+f4sxkdn1kp@users.noreply.github.com>
2022-12-15 13:26:32 -0500

    initial commit

---
+++ Iterative_Portfolio/Iterativeapp.js
@@ -0,0 +1,37 @@
+const hamburgerButton = document.getElementById("hamburger")
+const navList = document.getElementById("nav-list")
+const scrollTopButton = document.getElementById("scroll-top")
+
+function toggleButton() {
+    navList.classList.toggle("show")
+}
+
+hamburgerButton.addEventListener('click', toggleButton)
+
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+  if (document.body.scrollBottom > 5 || document.documentElement.scrollBottom > 5) {
+    mybutton.style.color = white;
+  } else {
+    mybutton.style.color = black;
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
---
+++ README.md
@@ -0,0 +1,13 @@
+Story: how do you want to present yourself
+Content: can a stranger understand the premise behind what you created
+Process: are you explaining your process to get to the end product
+
+Ask clarifying questions
+Create conversation
+Take notes
+Don't be shy
+
+Context!
+State the process
+
+
